3abbace987e599ff25040bc19b8397b6c085c75f
lucasnather <natherlucas@gmail.com>
2024-01-16 16:24:05 -0400

    feat: test e2e create video

--- src/controller/video/create.spec.ts
+++ src/controller/video/create.spec.ts
@@ -1,3 +1,35 @@
-test('a', () => {
-	console.log('oi')
+import { app } from '@/app'
+import request from 'supertest'
+
+describe('Create Videos [POST]', () => {
+
+	beforeEach(async () => {
+		await app.ready()
+	})
+
+	afterAll(async () => {
+		await app.close()
+	})
+
+	it('should be able to register a new video', async () => {
+		await request(app.server)
+			.post('/videos')
+			.send({
+				title: 'new title',
+				description: 'new description',
+				url: 'http://aaa.com.br'
+			})
+			.expect(201)
+	})
+
+	it('shouldn`t be able to register a new video with validation error', async () => {
+		await request(app.server)
+			.post('/videos')
+			.send({
+				title: 'ne',
+				description: 'new description',
+				url: 'ht://aaa.com.br'
+			})
+			.expect(401)
+	})
 })
